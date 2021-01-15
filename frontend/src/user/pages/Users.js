b522384cabc1cf8b7d23b3e211373b6c385b7ade
Jorge Eyzaguirre <jorgeyza@gmail.com>
2021-01-15 14:16:17 -0500

    Add Avatar and Card components && Add styles

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Jorge Eyzaguirre',
+      image:
+        'https://images.pexels.com/photos/1570610/pexels-photo-1570610.jpeg?cs=srgb&dl=pexels-paula-nardini-1570610.jpg&fm=jpg',
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
