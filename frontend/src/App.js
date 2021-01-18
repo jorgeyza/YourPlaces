04153a29b19acf6cd2508d3c1c3aaaa0192c46e2
Jorge Eyzaguirre <jorgeyza@gmail.com>
2021-01-18 15:27:35 -0500

    Add Auth Page and Login Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,10 @@ import {
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
@@ -29,6 +30,9 @@ const App = () => {
           </Route>
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>
