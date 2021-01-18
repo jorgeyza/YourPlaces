e8a9add52a0214361d9e144393edf4a49a89aafb
Jorge Eyzaguirre <jorgeyza@gmail.com>
2021-01-17 21:16:48 -0500

    Create Map component to display a Google Maps map

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+If you want to view the google maps, add the env variable REACT_APP_GOOGLE_API_KEY with a valid Google API key
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,4 +4,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const { REACT_APP_GOOGLE_API_KEY } = process.env;
+
+// Create the script tag, set the appropriate attributes
+var script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${REACT_APP_GOOGLE_API_KEY}`;
+script.defer = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function () {
+  // JS API is loaded and available
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
+
 ReactDOM.render(<App />, document.getElementById('root'));
