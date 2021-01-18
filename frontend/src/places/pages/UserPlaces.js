8cb16ca6f3b78b5465dfc920f39db3db90aa3b97
Jorge Eyzaguirre <jorgeyza@gmail.com>
2021-01-18 14:37:28 -0500

    Change dummy data && Fix logic when a place could not be found

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -19,7 +19,7 @@ const DUMMY_PLACES = [
   },
   {
     id: 'p2',
-    title: 'Empire State Building',
+    title: 'Emp. State Building',
     description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/250px-Empire_State_Building_from_the_Top_of_the_Rock.jpg',
