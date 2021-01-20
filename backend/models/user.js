1c620509920dc49d3856589403593ed620741f10
Jorge Eyzaguirre <jorgeyza@gmail.com>
2021-01-20 12:58:44 -0500

    Create User model

---
+++ backend/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, // unique creates an index in the db for faster querying
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  places: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator); //uniqueValidator makes sure the email does not repeat
+
+module.exports = mongoose.model('User', userSchema);
