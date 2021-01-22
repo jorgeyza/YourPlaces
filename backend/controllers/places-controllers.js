78b068bfe9901f661f422139f5c3afee916945e1
Jorge Eyzaguirre <jorgeyza@gmail.com>
2021-01-22 17:07:08 -0500

    Add authorization to create, update and delete places

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -142,6 +142,12 @@ const updatePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (place.creator.toString() !== req.userData.userId) {
+    // place.creator is an special object id, so we have to convert it to string first for the comparison
+    const error = new HttpError('You are not allowed to edit this place.', 401);
+    return next(error);
+  }
+
   place.title = title;
   place.description = description;
 
@@ -174,6 +180,11 @@ const deletePlace = async (req, res, next) => {
 
   if (!place) {
     const error = new HttpError('Could not find place for this id.', 404);
+    return next(error);
+  }
+
+  if (place.creator.id !== req.userData.userId) {
+    const error = new HttpError('You are not allowed to edit this place.', 401);
     return next(error);
   }
 
